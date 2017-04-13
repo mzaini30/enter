b7ccf913d3443442edcec4773d13572c39162c4f
mzaini30 <muhzaini30@gmail.com>
2017-04-13 11:30:59 +0800

    sip

--- app.js
+++ app.js
@@ -4,6 +4,6 @@ $proses = $(".proses")
 
 $proses.click(function(){
     $output.val($input.val().replace(/\n\n/g, "hdgvsgsgsg"))
-    $output.val($output.val().replace(/\n/g, ""))
+    $output.val($output.val().replace(/\n/g, " "))
     $output.val($output.val().replace(/hdgvsgsgsg/g, "\n"))
 })
